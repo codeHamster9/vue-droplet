71e57b2ecaf5d7cab7b0bbb1d339f2950935b9c9
Idan Sagi <idansagi@gmail.com>
2019-08-25 09:20:31 +0300

    select active kid in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,84 @@
 <template>
   <v-layout column
             justify-center>
-    <v-flex>
-      <img alt="Vue logo"
-           src="../assets/logo.png"
-           width>
+    <v-flex ma-5>
+      <span class="title">Select Kid</span>
     </v-flex>
     <v-flex>
-      <span class="title">Welcome To THe World!</span>
+      <v-item-group>
+        <v-layout row
+                  wrap
+                  justify-space-around>
+
+          <v-item v-for="kid in kids"
+                  :key="kid.id">
+            <v-card width="200"
+                    height="200"
+                    slot-scope="{ active, toggle}">
+              <v-layout column
+                        align-center
+                        justify-center
+                        fill-height
+                        :selected="active"
+                        @click="select(kid, toggle)">
+
+                <v-subheader>{{ kid.name }} : {{ kid.age }}</v-subheader>
+                <v-avatar size="72px"
+                          color="#033">
+                  <img v-if="kid.avatar"
+                       src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+                       alt="Avatar">
+                  <v-icon v-else
+                          medium
+                          :color="genderColor(kid.gender)">people</v-icon>
+                </v-avatar>
+              </v-layout>
+            </v-card>
+          </v-item>
+        </v-layout>
+      </v-item-group>
     </v-flex>
+
   </v-layout>
 </template>
 
 <script>
+import { mapState, mapActions, mapMutations } from 'vuex';
+import { types as KIDS } from '@/store/modules/kids';
+import { routeNames } from '@/router/route-names';
+
 export default {
   name: 'home',
+  mounted() {
+    this.fetch(this.userId);
+  },
+  computed: {
+    ...mapState('user', {
+      userId: state => Number(state.userId),
+    }),
+    ...mapState('kids', ['kids']),
+    genderColor() {
+      return gender => (gender.toLowerCase() === 'male' ? 'blue' : 'pink');
+    },
+  },
+  methods: {
+    ...mapMutations('kids', [KIDS.MUTATIONS.SET_ACTIVE]),
+    ...mapActions('kids', [KIDS.ACTIONS.FETCH]),
+    select(activeKid, toggleFn) {
+      this.setActive(activeKid.id);
+      toggleFn();
+      setTimeout(() => {
+        this.$router.push({ name: routeNames.SMILES });
+      }, 750);
+    },
+  },
 };
 </script>
+
+<style>
+.v-item--active {
+  border-radius: 25%;
+  border-color: aquamarine;
+  border: 5px solid var(--v-accent-base) !important;
+}
+</style>
