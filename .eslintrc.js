7593c3e4e1cb481e8ce6e62ea3a3efce2df51b29
Idan Sagi <idansagi@gmail.com>
2019-09-02 03:22:31 +0300

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+  extends: ["plugin:vue/essential", "airbnb-base", "@vue/airbnb"],
+  globals: {},
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
+    ecmaVersion: 2018,
+    sourceType: "module"
   },
+  plugins: ["vue"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  }
 };
